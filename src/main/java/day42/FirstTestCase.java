5526f15c6517d3714902783f3aedbbc452179898
Nidhi <nidhi.khurpia@gmail.com>
2024-07-30 23:21:45 -0700

    test

---
+++ src/main/java/day42/FirstTestCase.java
@@ -0,0 +1,29 @@
+package day42;
+
+import org.testng.annotations.Test;
+
+/*
+ 1) open app 
+ 2) Login
+ 3) Logout 
+ */
+
+public class FirstTestCase {
+	
+	@Test(priority = 4)
+	void opeapp() {
+		System.out.println("opening application...");
+		
+	}
+	
+	@Test(priority = 2)
+	void login() {
+		System.out.println("login to application...");
+	}
+	
+	@Test(priority=3)
+	void logout() {
+		System.out.println("logout from application...");
+	}
+
+}
