116e7801ca9c0d74348b4913bc1b81876774fcbd
nidhi <nidhi.khurpia@gmail.com>
2025-05-12 23:03:48 -0700

    commit-5/12/2025

--- src/main/java/Actions/ActionVsAction.java
+++ src/main/java/Actions/ActionVsAction.java
@@ -1,4 +1,4 @@
-package day35;
+package Actions;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,19 +14,19 @@ public class ActionVsAction {
 		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
 		driver.manage().window().maximize();
-		
+
 		WebElement button = driver.findElement(By.xpath("//span[@class='context-menu-one btn btn-neutral']"));
 		Actions act = new Actions(driver);
-		
+
 		Action myaction = act.contextClick(button).build(); //building creating an action and storing into a variable
-		
+
 		myaction.perform(); //we are performing/completing action
-		
-		
-		//Actiona -- class, will be used to perform mouse actions.
+
+
+		//Actions -- class, will be used to perform mouse actions.
 		//Action --- interface, will be used to store created actions.
 	}
-	
+
 	//https://jqueryscript.net/demo/Price-Range-slider-jQuery-UI/
 
 }
