55b25ee20c7183eeaf10330be02d6a7843733030
nidhi <nidhi.khurpia@gmail.com>
2025-05-13 11:29:24 -0700

    commit-5/13/2025

--- src/main/java/headlessMode/HeadlessTesting.java
+++ src/main/java/headlessMode/HeadlessTesting.java
@@ -1,4 +1,4 @@
-package day38;
+package headlessMode;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,21 +7,21 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class HeadlessTesting {
 
 	public static void main(String[] args) {
-		
+
 		ChromeOptions options = new ChromeOptions();
-		
+
 		options.addArguments("--headless=new"); //setting for headless mode of execution
-		
+
 		WebDriver driver = new ChromeDriver(options);
 		driver.get("https://demo.nopcommerce.com/");
-		
+
 		String act_title = driver.getTitle();
 		if(act_title.equals("your Store")) {
 			System.out.println("Test Passed");
 		}else {
 			System.out.println("Test failed");
 		}
-		
+
 		driver.quit();
 
 	}
