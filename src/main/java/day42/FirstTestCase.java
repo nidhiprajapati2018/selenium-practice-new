8dc4d22a62a87b9e7c2bfe627794dace137a90b5
nidhi <nidhi.khurpia@gmail.com>
2025-05-13 10:59:49 -0700

    commit-5/12/2025

--- src/main/java/day42/FirstTestCase.java
+++ src/main/java/day42/FirstTestCase.java
@@ -3,24 +3,27 @@ package day42;
 import org.testng.annotations.Test;
 
 /*
- 1) open app 
+ 1) open app
  2) Login
- 3) Logout 
+ 3) Logout
  */
 
 public class FirstTestCase {
 	
+
+
 	@Test(priority = 4)
 	void opeapp() {
 		System.out.println("opening application...");
 		
+
 	}
-	
+
 	@Test(priority = 2)
 	void login() {
 		System.out.println("login to application...");
 	}
-	
+
 	@Test(priority=3)
 	void logout() {
 		System.out.println("logout from application...");
