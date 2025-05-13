5c0136c92ab2e7ba5aa494615f192c184ed18226
nidhi <nidhi.khurpia@gmail.com>
2025-05-13 12:00:08 -0700

    commit-5/13/2025

--- src/main/java/day21/SeleniumWebElement.java
+++ src/main/java/day21/SeleniumWebElement.java
@@ -1,15 +1,14 @@
 package day21;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumWebElement {
+	
 
 	public static void main(String[] args) throws InterruptedException {
      //System.setProperty("webdrivwe.chrome.driver", "/Users/nidhiprajapati/Desktop/chromedriver_mac64");
-     WebDriver driver = new ChromeDriver();
-     driver.get("https://demo.opencart.com/");
+
+    // driver.get("https://demo.opencart.com/");
 //     driver.manage().window().maximize();
 //     Thread.sleep(2000);
 //     driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Poco F1");
@@ -18,7 +17,7 @@ public class SeleniumWebElement {
 //     driver.navigate().to("http://edureka.co/bog");
 //     driver.navigate().back();
 //     driver.quit();
-     
+
 
 	}
 
